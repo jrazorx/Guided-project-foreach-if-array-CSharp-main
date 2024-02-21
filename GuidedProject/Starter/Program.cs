15c5799b37e43badea1ccc3d59cc847cfd9ba3f2
jra <julien.gault.50@gmail.com>
2024-02-21 17:34:51 +0100

    Starter update with array + foreach for Sophia

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -2,7 +2,7 @@
 
 // initialize variables - graded assignments 
 int currentAssignments = 5;
-
+/* Original declarations for old way of calculating
 int sophia1 = 90;
 int sophia2 = 86;
 int sophia3 = 87;
@@ -26,32 +26,42 @@ int logan2 = 95;
 int logan3 = 87;
 int logan4 = 88;
 int logan5 = 96;
+*/
+int[] sophiaScores  = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores  = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores    = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores   = new int[] { 90, 95, 87, 88, 96 };
 
 int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
+// int andrewSum = 0;
+// int emmaSum = 0;
+// int loganSum = 0;
 
 decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
+foreach (int score in sophiaScores)
+{
+    // add the exam score to the sum
+    sophiaSum += score;
+}
+// decimal andrewScore;
+// decimal emmaScore;
+// decimal loganScore;
 
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+// sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+// andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
+// emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
+// loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
 
 sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
+// andrewScore = (decimal)andrewSum / currentAssignments;
+// emmaScore = (decimal)emmaSum / currentAssignments;
+// loganScore = (decimal)loganSum / currentAssignments;
 
 Console.WriteLine("Student\t\tGrade\n");
 Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+// Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+// Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+// Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
